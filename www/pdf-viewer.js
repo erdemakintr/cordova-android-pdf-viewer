ef423dd7d3c83e41abbc4083d210a5802fe7b17d
Jeremy Russell <jeremy.russell@anz.com>
2014-02-20 11:41:50 +1100

    fixing source file inclusions

--- www/pdf-viewer.js
+++ www/pdf-viewer.js
@@ -1,10 +1,10 @@
 var pdfViewerPlugin = {
-    createEvent: function(file, successCallback, errorCallback) {
+    openpdf: function(file, successCallback, errorCallback) {
         cordova.exec(
             successCallback, // success callback function
             errorCallback, // error callback function
             'PdfViewerPlugin', // mapped to our native Java class called "PdfViewerPlugin"
-            'viewPdf', // with this action name
+            'openpdf', // with this action name
             [{                  // and this array of custom arguments to create our entry
                 "file": file
             }]
